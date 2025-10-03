b81d9ddba6ff0c9ee3b92c1958a2cc85a0b62db1
labax03 <40730898@live.napier.ac.uk>
2025-10-03 15:43:55 +0100

    updated java app

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,9 +1,12 @@
+// Define package name
 package com.napier.sem;
 
+// Define a public class named App
 public class App
 {
     public static void main(String[] args)
     {
+        // Print message to the console
         System.out.println("Hello Group Project");
     }
 
