ead2fe27a89b4c3d83550b647688c72c822388d0
AndrewJohnMunn <40053954@live.napier.ac.uk>
2025-10-31 18:03:34 +0000

    Ran more general tests :)
    Signed-off-by: Andrew John Munn <40053954@live.napier.ac.uk>

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -6,6 +6,65 @@ class MyTest
     @Test
     void unitTest()
     {
-        assertEquals(5,5);
+        assertEquals(5,5, "Messages are equal");
+    }
+
+    @Test
+    void unitTest2()
+    {
+        assertEquals(5.0, 5.01, 0.02);
+    }
+
+    @Test
+    void unitTest3()
+    {
+        int[] a = {1,2,3};
+        int[] b = {1,2,3};
+        assertArrayEquals(a,b);
+    }
+
+    @Test
+    void unitTest4()
+    {
+        assertTrue(5==5);
+    }
+
+    @Test
+    void unitTest5()
+    {
+        assertFalse(5==4);
+    }
+
+    @Test
+    void unitTest6()
+    {
+        assertNull(null);
+    }
+
+    @Test
+    void unitTest7()
+    {
+    }
+    @Test
+    void unitTest8()
+    {
+        assertNotNull("Hello");
+    }
+
+    @Test
+    void unitTest9()
+    {
+        assertThrows(NullPointerException.class, this::throwsException);
+    }
+
+    void throwsException()
+    {
+        throw new NullPointerException();
+    }
+
+    @Test
+    void unitTest10() throws NullPointerException
+    {
+       throw new NullPointerException();
     }
 }
