52dbe8cf5107bae448077b243128f57564338a93
AndrewJohnMunn <40053954@live.napier.ac.uk>
2025-11-07 13:13:35 +0000

    Altered Dockerfile in relation to Kevin's email.
    Added and tested code to CountryTest, and object creation code to Country.java
    
    Signed-off-by: Andrew John Munn <40053954@live.napier.ac.uk>

--- src/main/java/com/napier/sem/Country.java
+++ src/main/java/com/napier/sem/Country.java
@@ -1,4 +1,37 @@
 package com.napier.sem;
 
 public class Country {
+    private String code;
+    private String name;
+    private String continent;
+    private String region;
+    private int population;
+    private int capital;
+
+    // ✅ Constructor
+    public Country(String code, String name, String continent, String region, int population, int capital) {
+        this.code = code;
+        this.name = name;
+        this.continent = continent;
+        this.region = region;
+        this.population = population;
+        this.capital = capital;
+    }
+
+    // Getters
+    public String getCode() { return code; }
+    public String getName() { return name; }
+    public String getContinent() { return continent; }
+    public String getRegion() { return region; }
+    public int getPopulation() { return population; }
+    public int getCapital() { return capital; }
+
+    @Override
+    public String toString() {
+        return String.format("%s | %s | %s | %s | %d | %d",
+                code, name, continent, region, population, capital);
+    }
+
+
 }
+
